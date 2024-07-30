1ef784b9578dcd1b4d1f9a3c6f9b263fb885f12c
1johnwick001 <john.wick00000456@gmail.com>
2024-07-30 10:55:57 +0530

    updated code with gallery module

--- institute/api_urls.py
+++ institute/api_urls.py
@@ -4,4 +4,7 @@ from .api_views import *
 urlpatterns = [
     path('notifications/', notification_list, name='notification_list'),
     path('notifications/<int:pk>/', notification_detail, name='notification_detail'),
+    
+    # url for gallery
+    path('gallery/', gallery_list, name='gallery_list'),
 ]
--- institute/serializers.py
+++ institute/serializers.py
@@ -5,3 +5,9 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationModel
         fields = "__all__"
+        
+class GallerySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = GalleryItems
+        fields = ['name','image','video','url_tag']
